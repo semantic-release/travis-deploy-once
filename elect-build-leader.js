baed0b95d3002130087b0cb0207a53e33fdb636b
Stephan Bönnemann <stephan@boennemann.me>
2017-08-17 21:48:41 +0200

    feat: elect build leader by highest node version
    BREAKING CHANGE: The first job in the build matrix is no longer
    automatically elected to be the build leader.
    Instead the build job with the highest node version is selected.
    If you want to control which job is the build leader set the
    environment variable `BUILD_LEADER_ID`.

---
+++ elect-build-leader.js
@@ -0,0 +1,23 @@
+const semver = require('semver')
+
+module.exports = versions => {
+  // if there is only one candidate, then it's the winner
+  if (!Array.isArray(versions) || versions.length === 1) return 1
+
+  // if there is latest stable it's the winner
+  const stable = versions.indexOf('node') + 1
+  if (stable) return stable
+
+  // otherwise we use the lower bound of all valid semver ranges
+  const validRanges = versions.filter(semver.validRange)
+  const lowVersionBoundaries = validRanges
+  .map(semver.Range)
+  .map(r => r.set[0][0].semver.version)
+
+  // then we find the highest of those
+  const highestVersion = semver.sort(Array.from(lowVersionBoundaries)).pop()
+  const highestRange = validRanges[lowVersionBoundaries.indexOf(highestVersion)]
+
+  // and make its build job the winner
+  return versions.indexOf(highestRange) + 1
+}
