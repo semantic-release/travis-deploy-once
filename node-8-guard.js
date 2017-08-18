85ebe13c560e8e6ce808f78f2ebd557e00d02cf3
Stephan Bönnemann <stephan@boennemann.me>
2017-08-18 10:48:41 +0200

    fix: log error when exiting on node < 8

--- node-8-guard.js
+++ node-8-guard.js
@@ -1,5 +1,8 @@
 var semver = require('semver')
 
 module.exports = semver.lt(process.version, '8.0.0')
-  ? function () { return null }
+  ? function () {
+    console.error(`Node with ${process.version} can't be the build leader. Node 8 and up required.`)
+    return null
+  }
   : require('./index')
