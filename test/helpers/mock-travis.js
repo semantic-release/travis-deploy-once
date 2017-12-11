65259a623ee5a6cbeddaa54a35f81daeff94baa9
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2017-12-11 00:27:46 -0500

    feat: replace Travis API client

--- test/helpers/mock-travis.js
+++ test/helpers/mock-travis.js
@@ -2,12 +2,24 @@ import nock from 'nock';
 
 /* eslint camelcase: ["error", {properties: "never"}] */
 
-export function authenticate({GH_TOKEN = process.env.GH_TOKEN, pro = false} = {}) {
-  return nock(pro ? 'https://api.travis-ci.com' : 'https://api.travis-ci.org', {reqheaders: {'user-agent': 'Travis'}})
-    .post('/auth/github', {github_token: GH_TOKEN})
-    .reply(200, {access_token: 'TRAVIS_TOKEN'})
-    .get('/users')
-    .reply(200);
+export function authenticate(
+  {travisOpts: {pro = false, enterprise} = {}, travisToken = 'TRAVIS_TOKEN', githubToken = process.env.GH_TOKEN} = {}
+) {
+  return nock(enterprise || (pro ? 'https://api.travis-ci.com' : 'https://api.travis-ci.org'), {
+    reqheaders: {'user-agent': 'Travis'},
+  })
+    .post('/auth/github', {github_token: githubToken})
+    .reply(200, {access_token: travisToken});
+}
+
+export function api({travisOpts: {pro = false, enterprise} = {}, travisToken = 'TRAVIS_TOKEN'} = {}) {
+  return nock(enterprise || (pro ? 'https://api.travis-ci.com' : 'https://api.travis-ci.org'), {
+    reqheaders: {
+      'user-agent': 'Travis',
+      accept: 'application/vnd.travis-ci.2+json',
+      authorization: `token ${travisToken}`,
+    },
+  });
 }
 
 export function unauthorized() {
--- test/wait-for-other-jobs.test.js
+++ test/wait-for-other-jobs.test.js
@@ -2,9 +2,8 @@ import test from 'ava';
 import nock from 'nock';
 import {stub} from 'sinon';
 import getLogger from '../lib/get-logger';
-import getClient from '../lib/get-client';
 import waitForOtherJobs from '../lib/wait-for-other-jobs';
-import {authenticate} from './helpers/mock-travis';
+import {api} from './helpers/mock-travis';
 
 test.beforeEach(t => {
   t.context.env = process.env;
@@ -21,6 +20,8 @@ test.afterEach.always(t => {
 });
 
 test.serial('Wait for jobs to complete, ignoring current one and return true', async t => {
+  const travisToken = 'TRAVIS_TOKEN';
+  const travisOpts = {};
   const buildId = 123;
   const currentJobId = 2;
   const jobsFirst = [
@@ -38,17 +39,14 @@ test.serial('Wait for jobs to complete, ignoring current one and return true', a
     {id: currentJobId, number: '1.2', state: 'started'},
     {id: 3, number: '1.3', state: 'passed'},
   ];
-  const travis = authenticate();
-  const client = await getClient({}, process.env);
-
-  travis
+  const travis = api()
     .get(`/builds/${buildId}`)
     .reply(200, {jobs: jobsSecond})
     .get(`/builds/${buildId}`)
     .reply(200, {jobs: jobsThird});
 
   t.true(
-    await waitForOtherJobs(client, buildId, currentJobId, jobsFirst, t.context.logger, {
+    await waitForOtherJobs(travisOpts, travisToken, buildId, currentJobId, jobsFirst, t.context.logger, {
       forever: true,
       factor: 1,
       minTimeout: 1,
@@ -63,6 +61,8 @@ test.serial('Wait for jobs to complete, ignoring current one and return true', a
 });
 
 test.serial('Return false as soon as a job fails', async t => {
+  const travisToken = 'TRAVIS_TOKEN';
+  const travisOpts = {};
   const buildId = 123;
   const currentJobId = 2;
   const jobsFirst = [
@@ -80,17 +80,14 @@ test.serial('Return false as soon as a job fails', async t => {
     {id: currentJobId, number: '1.2', state: 'started'},
     {id: 3, number: '1.3', state: 'errored'},
   ];
-  const travis = authenticate();
-  const client = await getClient({}, process.env);
-
-  travis
+  const travis = api()
     .get(`/builds/${buildId}`)
     .reply(200, {jobs: jobsSecond})
     .get(`/builds/${buildId}`)
     .reply(200, {jobs: jobsThird});
 
   t.false(
-    await waitForOtherJobs(client, buildId, currentJobId, jobsFirst, t.context.logger, {
+    await waitForOtherJobs(travisOpts, travisToken, buildId, currentJobId, jobsFirst, t.context.logger, {
       forever: true,
       factor: 1,
       minTimeout: 1,
@@ -106,6 +103,8 @@ test.serial('Return false as soon as a job fails', async t => {
 });
 
 test.serial('Handle API errors', async t => {
+  const travisToken = 'TRAVIS_TOKEN';
+  const travisOpts = {};
   const buildId = 123;
   const currentJobId = 2;
   const jobsFirst = [
@@ -118,17 +117,14 @@ test.serial('Handle API errors', async t => {
     {id: currentJobId, number: '1.2', state: 'started'},
     {id: 3, number: '1.3', state: 'passed'},
   ];
-  const travis = authenticate();
-  const client = await getClient({}, process.env);
-
-  travis
+  const travis = api()
     .get(`/builds/${buildId}`)
     .reply(500, 'server error')
     .get(`/builds/${buildId}`)
     .reply(200, {jobs: jobsSecond});
 
   t.true(
-    await waitForOtherJobs(client, buildId, currentJobId, jobsFirst, t.context.logger, {
+    await waitForOtherJobs(travisOpts, travisToken, buildId, currentJobId, jobsFirst, t.context.logger, {
       forever: true,
       factor: 1,
       minTimeout: 1,
@@ -136,7 +132,10 @@ test.serial('Handle API errors', async t => {
   );
 
   t.is(t.context.log.firstCall.args[0], 'Aborting attempt 1, because of pending job(s): 1.1, 1.3.');
-  t.is(t.context.log.secondCall.args[0], 'Failed attempt 2, because Travis API returned the error: server error.');
+  t.is(
+    t.context.log.secondCall.args[0],
+    'Failed attempt 2, because Travis API returned the error: HTTPError: Response code 500 (Internal Server Error).'
+  );
   t.is(t.context.log.thirdCall.args[0], 'Success at attempt 3. All 3 jobs passed.');
   t.true(travis.isDone());
 });
