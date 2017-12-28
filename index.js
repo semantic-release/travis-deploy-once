f185ab75546590139daa73c778676139af5421f7
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2017-12-27 22:41:48 -0500

    feat: support node >=4

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-module.exports = require('semver').lt(process.version, '8.0.0')
-  ? function() {
-      console.error(`Node with ${process.version} can't be the build leader. Node 8 and up required.`);
-      return null;
-    }
-  : require('./lib/travis-deploy-once');
+#!/usr/bin/env node
+/* eslint-disable import/no-unassigned-import */
+require('babel-register')({only: 'travis-deploy-once/lib'});
+require('babel-polyfill');
+
+module.exports = require('./lib/travis-deploy-once');
