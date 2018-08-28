ebda85c33ca400219ce9809df8504f46affdb4ae
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2018-08-28 17:04:29 -0400

    fix(package): update babel to version 7.0.0

--- bin/travis-deploy-once.js
+++ bin/travis-deploy-once.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
 
 /* eslint-disable import/no-unassigned-import */
-require('babel-register')({
-  only: /travis-deploy-once\/(lib|cli.js)|travis-deploy-once\/node_modules\/(got|cacheable-request)/,
-});
-require('babel-polyfill');
+require('../babel-register');
 
 require('../cli')().catch(() => {
   process.exitCode = 1;
