1730ee3c81d20bae8736c8f9600edcccbe811e78
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2018-09-19 11:51:14 -0400

    fix: fix babel-register config when module is globally installed

--- babel-register.js
+++ babel-register.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-unassigned-import */
 require('@babel/register')({
-  only: ['lib', 'cli.js', 'node_modules/got', 'node_modules/cacheable-request'],
+  only: [/travis-deploy-once\/(lib|cli.js)/, /node_modules\/(got|cacheable-request)/],
   presets: [['@babel/preset-env', {targets: {node: 'current'}, useBuiltIns: 'entry'}]],
 });
 require('@babel/polyfill');
